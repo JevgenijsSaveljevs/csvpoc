302fd9ac7b4560078abd26fccc84ae453fe444b9
eugene <banderge@hotmail.com>
2023-11-06 10:03:39 +0200

    wip

---
+++ CsvPoc/CloudFrontParserNew.cs
@@ -0,0 +1,169 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Buffers;
+using System.IO;
+using System.IO.Pipelines;
+using System.Text;
+
+public static class FancyStreamReader
+{
+    private static ReadOnlySpan<byte> NewLine => Encoding.UTF8.GetBytes("\r\n");
+
+    public static async Task ReadStream(PipeReader pipeReader, List<Person> items, int batchApproximateSize = 1_000)
+    {
+        var position = 0;
+        var parsedCount = 0;
+        ReadResult lastReadResult = default;
+        while (true)
+        {
+            var result = await pipeReader.ReadAsync();
+            var buffer = result.Buffer;
+            var sequencePosition = ParseLines(items, ref buffer, ref position, ref parsedCount);
+
+            pipeReader.AdvanceTo(sequencePosition, buffer.End);
+
+            if (result.IsCompleted || parsedCount >= batchApproximateSize)
+            {
+                break;
+            }
+        }
+
+        if (lastReadResult.IsCompleted)
+        {
+            pipeReader.Complete();
+        }
+    }
+
+    public static async IAsyncEnumerable<List<Person>> ReadStreamAsAsyncEnumerable(PipeReader pipeReader, int batchApproximateSize = 1_000)
+    {
+        ReadResult lastReadResult = default;
+        do
+        {
+            var position = 0;
+            var parsedCount = 0;
+            
+            var items = new List<Person>();
+            while (true)
+            {
+                var result = await pipeReader.ReadAsync();
+                lastReadResult = result;
+                var buffer = result.Buffer;
+                var sequencePosition = ParseLines(items, ref buffer, ref position, ref parsedCount);
+
+                pipeReader.AdvanceTo(sequencePosition, buffer.End);
+
+                if (result.IsCompleted || parsedCount >= batchApproximateSize)
+                {
+                    break;
+                }
+            }
+
+            if (lastReadResult.IsCompleted)
+            {
+                pipeReader.Complete();
+                yield return items;
+            }
+
+            if (items.Count >= 1000)
+            {
+                yield return items;
+            } 
+
+        } while (!lastReadResult.IsCompleted);
+    }
+
+    private static SequencePosition ParseLines(
+        List<Person> itemsArray,
+        ref ReadOnlySequence<byte> buffer,
+        ref int position,
+        ref int parsedCount)
+    {
+        var reader = new SequenceReader<byte>(buffer);
+
+        while (!reader.End)
+        {
+            if (!reader.TryReadToAny(out ReadOnlySpan<byte> line, NewLine, true))
+            {
+                break; // we don't have a delimiter (newline) in the current data
+            }
+
+            var parsedLine = new Person();
+            //itemsArray[position++] = parsedLine;
+            itemsArray.Add(parsedLine);
+            parsedCount++;
+        }
+
+        return reader.Position;
+    }
+}
+
+public static class CloudFrontParserNew
+{
+    private static ReadOnlySpan<byte> NewLine => Encoding.UTF8.GetBytes("\r\n");//  "\r\n"u8;
+
+    public static async Task<int> ParseAsync(string filePath, Person[] items)
+    {
+        var position = 0;
+
+        if (File.Exists(filePath))
+        {
+            await using var fileStream = File.OpenRead(filePath);
+
+            var pipeReader = PipeReader.Create(fileStream);
+
+            while (true)
+            {
+                var result = await pipeReader.ReadAsync();
+
+                var buffer = result.Buffer;
+
+                var sequencePosition = ParseLines(items, ref buffer, ref position);
+
+                pipeReader.AdvanceTo(sequencePosition, buffer.End);
+
+                if (result.IsCompleted)
+                {
+                    break;
+                }
+            }
+
+            pipeReader.Complete();
+        }
+
+        return position;
+    }
+
+    private static SequencePosition ParseLines(Person[] itemsArray, 
+        ref ReadOnlySequence<byte> buffer, ref int position)
+    {
+        var reader = new SequenceReader<byte>(buffer);
+
+        while (!reader.End)
+        {
+            if (!reader.TryReadToAny(out ReadOnlySpan<byte> line, NewLine, true))
+            {
+                break; // we don't have a delimiter (newline) in the current data
+            }
+
+            var parsedLine = LineParser.ParseLine(line); // we have a line to parse
+
+            //if (parsedLine.HasValue) 
+                //itemsArray[position++] = parsedLine.Value;
+                itemsArray[position++] = parsedLine;
+        }
+
+        return reader.Position;
+    }
+
+    private static class LineParser
+    {
+        private const byte Tab = (byte)'\t', Hash = (byte)'#';
+
+        public static Person? ParseLine(ReadOnlySpan<byte> line)
+        {
+            var str = Encoding.UTF8.GetString(line);
+            var lines = str.Split(";");
+            return new Person();
+        }
+    }
+}
--- CsvPoc/Program.cs
+++ CsvPoc/Program.cs
@@ -2,34 +2,100 @@
 
 using CsvPoc;
 using System.Buffers;
+using System.IO.Pipelines;
+using System.Reflection.PortableExecutable;
+using System.Runtime.InteropServices;
+using static Bogus.DataSets.Name;
 
 Console.WriteLine("Hello, World!");
 //await CsvGenerator.Generate("D:\\Soft\\cocojambo.csv", 500, 10_000);
 
-using var fs = File.Open("D:\\Soft\\cocojambo.csv", FileMode.Open);
-using var sr = new StreamReader(fs);
-var arr = new char[512];
-var mem = new Memory<char>(arr);
-var chunk = await sr.ReadAsync(mem);
-Console.WriteLine(mem.Length);
-Console.WriteLine(mem.Span.ToString());
-Console.WriteLine();
-Console.WriteLine("================");
-Console.WriteLine();
+string fileName = "D:\\Soft\\cocojambo.csv";
 
-await sr.ReadAsync(mem);
-Console.WriteLine(mem.Length);
-Console.WriteLine(mem.Span.ToString());
+//using var fs = File.Open(fileName, FileMode.Open);
+//using var sr = new StreamReader(fs);
+//var arr = new char[512];
+//var mem = new Memory<char>(arr);
+//var chunk = await sr.ReadAsync(mem);
+//Console.WriteLine(mem.Length);
+//Console.WriteLine(mem.Span.ToString());
+//Console.WriteLine();
+//Console.WriteLine("================");
+//Console.WriteLine();
 
-Console.WriteLine();
-Console.WriteLine("================");
-Console.WriteLine();
+//await sr.ReadAsync(mem);
+//Console.WriteLine(mem.Length);
+//Console.WriteLine(mem.Span.ToString());
 
-await sr.ReadAsync(mem);
-Console.WriteLine(mem.Length);
-Console.WriteLine(mem.Span.ToString());
+//Console.WriteLine();
+//Console.WriteLine("================");
+//Console.WriteLine();
+
+//await sr.ReadAsync(mem);
+//Console.WriteLine(mem.Length);
+//Console.WriteLine(mem.Span.ToString());
 
 
 //var memory = new Memory<char>();
 //var r = memory.Span.IndexOf(Environment.NewLine);
 //var chunk = sr.ReadAsync(memory);
+
+//var list  = new List<char>();
+//var spans = CollectionsMarshal.AsSpan(list);
+//Span<char> c = new Span<char>();
+//c.ad
+//spans.Slice()
+
+//using var fs = File.Open(fileName, FileMode.Open);
+//var reader = PipeReader.Create(fs);
+//var writer = PipeWriter.Create(
+//           Console.OpenStandardOutput(),
+//           new StreamPipeWriterOptions(leaveOpen: true));
+
+//ReadResult readResult = await reader.ReadAsync();
+//ReadOnlySequence<byte> buffer = readResult.Buffer;
+//readResult.Buffer.
+
+//Console.WriteLine(buffer);
+////var pipe = new Pipe();
+////PipeReader reader = pipe.Reader;
+////PipeWriter writer = pipe.Writer;
+
+
+
+//using var fs = File.Open(fileName, FileMode.Open);
+//using var sr = new StreamReader(fs);
+//fs.ReadAsync()
+
+// demoware - this prototype example doesn't cover all edge cases!
+var result = new List<Person>();
+//var result = await CloudFrontParserNew.ParseAsync(fileName, res);
+
+using var fs = File.Open(fileName, FileMode.Open);
+await foreach (var batch in FancyStreamReader.ReadStreamAsAsyncEnumerable(PipeReader.Create(fs)))
+{
+    Console.WriteLine($"done: {batch.Count}");
+}
+
+
+
+
+//await foreach (var p in FunnyPrinter.GetPersions())
+//{
+//    Console.WriteLine(p);
+//}
+
+
+//public class FunnyPrinter
+//{
+//    public static async IAsyncEnumerable<Person> GetPersions()
+//    {
+//        await Task.Delay(10);
+//        yield return new Person();
+//        yield return new Person();
+//        yield return new Person();
+//        yield return new Person();
+//        yield return new Person();
+//        //yield break;
+//    }
+//}
